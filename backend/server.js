28b20ec950546c78ea39f998baa090f1b3ad4a24
Akshay Tiwlekar <akstiwle@iu.edu>
2023-05-20 18:06:45 -0400

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log("Server...");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5500;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// to get the data from the request body instead of undefined
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
